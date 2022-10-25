fd3e16fb427ec23a8c55ff1b3a9df5259cf3bc70
jgabruel <joaogg357@gmail.com>
2022-10-25 18:35:18 -0300

    tipos variaveis modulo completo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,35 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.500;
     }
 }
+
+
+/*
+ TIPOS E VARIAVEIS
+
+Tipos de dados
+Tipos primitivos
+int, byte, short, long, float, double, boolean e char
+MAIORIA DOS CENARIOS VAI SE USAR INT
+
+Os tipos primitivos tambem pode contar partes fracionárias e podem ser representadas
+por 2 tipos
+- float
+- double DOUBLE MAIS USADO
+
+Declaracao de variaveis
+<tipo> <nomeVariavel> <valoropcional>
+int idade;
+int anoFabricacao = 2021;
+double salarioMinimo = 2.500;
+
+Variáveis e Constantes
+Contantes = valores aramazenados na memoria que nao podem ser modificados depois de declarados
+Rpresentados pela palavra reservada final
+tem que estar em caixa alta
+
+final VALOR_DE_PI=3.15 
+
+
+ */

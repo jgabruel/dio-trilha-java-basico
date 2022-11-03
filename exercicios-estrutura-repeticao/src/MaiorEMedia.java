3da75cd8f8c6c1aa1b49d5a91fc4e6d65cfff3ba
jgabruel <joaogg357@gmail.com>
2022-11-03 18:54:14 -0300

    exercicios while/do-while/for

---
+++ exercicios-estrutura-repeticao/src/MaiorEMedia.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MaiorEMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int count = 0;
+        int maior = 0;
+        int soma = 0;
+
+        do {
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+            soma = soma + numero;
+
+            if (numero > maior) maior = numero;
+
+            count = count + 1;
+        } while(count < 5);
+        System.out.println("Maior numero: " + maior);
+        System.out.println("Media dos nmeros: " + (soma/5));
+
+    }
+
+}
---
+++ exercicios-estrutura-repeticao/src/ParEImpar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int quantNumeros;
+        int numero;
+        int quantPares = 0, quantImpares =0;
+        System.out.println("Quantos numeros vai passar?: ");
+        quantNumeros = scan.nextInt();
+        
+        int count = 0;
+        do{
+            System.out.println("Numero: ");
+            numero = scan.nextInt();
+            if (numero % 2 == 0) quantPares++;
+            else quantImpares++;
+            count = count + 1;
+        } while(count < quantNumeros);
+
+        System.out.println("Quantidade de pares: "+ quantPares);
+        System.out.println("Quantidade de impares: " + quantImpares);
+    }
+}
